ea701c592762b0f52181dda23d6f19b2398f7d14
makitito <emmanuel.nainye@strathmore.edu>
2016-04-22 08:50:22 +0300

    Added files via upload

---
+++ Rewriting Factorial II.cpp
@@ -0,0 +1,64 @@
+C++ shell
+cpp.sh
+online C++ compiler
+about cpp.sh
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+#include <iostream> 
+using namespace std;
+int main ()
+        {
+            int number ;
+            cout << " Enter a number :";
+            cin >> number ;
+            switch ( number )
+                {
+                    case 0:
+                    case 1:
+                        cout << "1\n";
+                        break ;
+                    
+                    case 2:
+                        cout << "2\n";
+                        break ;
+                    
+                    case 3:
+                        cout << "6\n";
+                        break ;
+                    
+                    case 4:
+                        cout << "24\n";
+                        break ;
+RunGet URL
+optionscompilationexecution
+ Enter a number :10
+3628800
+ 
+Exit code: 0 (normal program termination)
+C++ Shell, 2014-2015
---
+++ do...while loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int N;
+
+	cin >> N;
+
+    do
+		{
+		cout<<"Hello ,World !\n";
+		}
+
+	while (-- N > 0);
+
+	return 0;
+}
